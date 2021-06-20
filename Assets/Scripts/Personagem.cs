7d7aa1ece0d08b21d0a6d305898a949c425ec112
Henrique <henriquecunhalopes@yahoo.com.br>
2021-06-19 22:11:12 -0400

    Movimento horizontal adicionado

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    Rigidbody2D rb;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Andar();
+        float yMov = Input.GetAxisRaw("Vertical") * speed;
+        rb.velocity = new Vector2(rb.velocity.x, yMov);
+    }
+    void Andar()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal") * speed;
+        rb.velocity = new Vector2(xMov, rb.velocity.y);
     }
 }
